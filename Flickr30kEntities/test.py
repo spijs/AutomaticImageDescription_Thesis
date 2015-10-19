db0e30546fc96030909748a0035a0b33c9f74d97
Wout <w.vekemans@gmail.com>
2015-10-19 22:53:43 +0200

    sentence + image parsing complete

--- Flickr30kEntities/test.py
+++ Flickr30kEntities/test.py
@@ -1,49 +1,51 @@
-import matlab.engine
 import os
 import random
 import scipy.io
 import codecs
 import numpy
+from PIL import Image
 
 from collections import defaultdict
 
-eng = matlab.engine.start_matlab()
-for dirname, dirnames, filenames in os.walk('./tmp'):
-    # print path to all subdirectories first.
+for dirname, dirnames, filenames in os.walk('./matFiles'):
+    # # print path to all subdirectories first.
     for filename in filenames:
-        eng.getAnnotations(os.path.join(dirname, filename), nargout = 0)
-        annotations = scipy.io.loadmat('annotations.mat')
+    	fileToLoad = os.path.join('matFiles',filename[0:len(filename)])
+    	imgpath = 'G:/flickr30k-images/flickr30k-images/' + filename[0:(len(filename)-4)] + '.jpg'
+    	img = Image.open(imgpath)
+        annotations = scipy.io.loadmat(fileToLoad)
         annotation_struct = annotations['annotations'][0][0]
-        img = annotation_struct[0][0]
-        print img
+        imgname = annotation_struct[0][0]
+        # print imgname
+        # # print img
         boxIDs = annotation_struct[3]
         dimensions = annotation_struct[4]
         mapping = annotation_struct[5]
-        print len(boxIDs)
         boxid_extracted = []
         for i in range(len(boxIDs)):
-        	newid = boxIDs[i][0][0]
-        	boxid_extracted.append(newid)
-        print dimensions
-# print mapping[0][0]
-# eng = matlab.engine.start_matlab()
-# print len(eng.getAnnotations('Annotations/36979.xml'))
+            newid = boxIDs[i][0][0]
+            boxid_extracted.append(newid)
 
-# print struct_read
-
-
-# annotation_struct = struct_read['ann1'][0][0]
-
-# img = annotation_struct[0][0]
-# boxIDs = annotation_struct[3]
-# dimensions = annotation_struct[4]
-# mapping = annotation_struct[5]
-
-# print img
-# print len(boxIDs)
-# boxid_extracted = []
-# for i in range(len(boxIDs)):
-# 	newid = boxIDs[i][0][0]
-# 	boxid_extracted.append(newid)
-# print dimensions
-# print mapping[0][0]
+        for i in range(len(boxid_extracted)):
+            curr_map = mapping[i][0]
+            curr_map = [item for sublist in curr_map for item in sublist]
+            # print 'map', curr_map, 'length', len(curr_map)
+            if len(curr_map) > 0:
+                # print 'length mapping', len(curr_map), 'mapp', curr_map
+                # print 'dimensions', dimensions
+                for j in range(len(curr_map)):
+                    # print 'J', j
+                    # print curr_map[j]
+                    label = dimensions[curr_map[j]-1][0][0]
+                    # print 'LABEL', label
+                    while len(label) ==1:
+                      label = label[0]
+                    if len(label) != 0:
+                        box = (label[0], label[1], label[2], label[3])
+                        region = img.crop(box)
+                        regionsave = 'image_snippets/' + str(imgname) + '_' + boxid_extracted[i] +'_'+str(j+1) + '.jpg'
+                        # # print 'regionsave', regionsave 
+                        region.save(regionsave)
+            
+        # # print dimensions[0][0][0][0]
+        # # print boxid_extracted[0]
