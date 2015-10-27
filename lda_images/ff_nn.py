300152bc5540eff467d2dc4d61cd158a8ea5311b
Wout <w.vekemans@gmail.com>
2015-10-27 11:47:11 +0100

    nul layers G

--- lda_images/ff_nn.py
+++ lda_images/ff_nn.py
@@ -19,72 +19,78 @@ class FeedForwardNetwork:
         self.correct = zeros((self.nOut,1), dtype=float)
          
         # initialize weights randomly (+1 for bias)
-        self.hWeights1 = initw(self.nHidden, self.nIn+1)
-        self.hWeights2 = initw(self.nHidden, self.nHidden+1)
-        #self.hWeights3 = random.random((self.nHidden, self.nHidden+1))
-	# print 'HWEIGHT', self.hWeights
-        self.oWeights = initw(self.nOut, self.nHidden+1)
-         
-        # activations of neurons (sum of inputs)
-        self.hActivation1 = zeros((self.nHidden, 1), dtype=float)
-        self.hActivation2 = zeros((self.nHidden, 1), dtype=float)
-        #self.hActivation3 = zeros((self.nHidden, 1), dtype=float)
-        self.oActivation = zeros((self.nOut, 1), dtype=float)
-         
-        # outputs of neurons (after sigmoid function)
-        self.iOutput = zeros((self.nIn+1, 1), dtype=float)      # +1 for bias
-        self.hOutput1 = zeros((self.nHidden+1, 1), dtype=float)  # +1 for bias
-        self.hOutput2 = zeros((self.nHidden+1, 1), dtype=float)  # +1 for bias
-        #self.hOutput3 = zeros((self.nHidden+1, 1), dtype=float)  # +1 for bias
-        self.oOutput = zeros((self.nOut,1), dtype=float)
-         
-        # deltas for hidden and output layer
-        self.hDelta1 = zeros((self.nHidden), dtype=float)
-        self.hDelta2 = zeros((self.nHidden), dtype=float)
-        #self.hDelta3 = zeros((self.nHidden), dtype=float)
+     #    self.hWeights1 = initw(self.nHidden, self.nIn+1)
+     #    self.hWeights2 = initw(self.nHidden, self.nHidden+1)
+     #    #self.hWeights3 = random.random((self.nHidden, self.nHidden+1))
+	# # print 'HWEIGHT', self.hWeights
+     #    self.oWeights = initw(self.nOut, self.nHidden+1)
+     #
+     #    # activations of neurons (sum of inputs)
+     #    self.hActivation1 = zeros((self.nHidden, 1), dtype=float)
+     #    self.hActivation2 = zeros((self.nHidden, 1), dtype=float)
+     #    #self.hActivation3 = zeros((self.nHidden, 1), dtype=float)
+     #    self.oActivation = zeros((self.nOut, 1), dtype=float)
+     #
+     #    # outputs of neurons (after sigmoid function)
+     #    self.iOutput = zeros((self.nIn+1, 1), dtype=float)      # +1 for bias
+     #    self.hOutput1 = zeros((self.nHidden+1, 1), dtype=float)  # +1 for bias
+     #    self.hOutput2 = zeros((self.nHidden+1, 1), dtype=float)  # +1 for bias
+     #    #self.hOutput3 = zeros((self.nHidden+1, 1), dtype=float)  # +1 for bias
+     #    self.oOutput = zeros((self.nOut,1), dtype=float)
+     #
+     #    # deltas for hidden and output layer
+     #    self.hDelta1 = zeros((self.nHidden), dtype=float)
+     #    self.hDelta2 = zeros((self.nHidden), dtype=float)
+     #    #self.hDelta3 = zeros((self.nHidden), dtype=float)
+     #    self.oDelta = zeros((self.nOut,1), dtype=float)
+        self.weights = initw(self.nOut, self.nIn+1)
+        self.iOutput = zeros((self.nIn+1, 1), dtype=float)
+        self.activation = zeros((self.nOut, 1), dtype= float)
         self.oDelta = zeros((self.nOut,1), dtype=float)
      
     def forward(self, input):
         # set input as output of first layer (bias neuron = 1.0)
         self.iOutput[:-1, 0] = input
         self.iOutput[-1:, 0] = 1.0
-        
+
+        self.activation = dot(self.weights, self.iOutput)
 	# print 'iOutput', self.iOutput
  
-        # hidden layer
-        self.hActivation1 = dot(self.hWeights1, self.iOutput)/self.nHidden
-        self.hOutput1[:-1, :] = tanh(self.hActivation1)
-	#print 'input eerste layer', self.hActivation1
-        self.hOutput1[-1:, :] = 1.0
-
-        self.hActivation2 = dot(self.hWeights2, self.hOutput1)/self.nHidden
-        self.hOutput2[:-1, :] = tanh(self.hActivation2)
-        self.hOutput2[-1:, :] = 1.0
-
-        #self.hActivation3 = dot(self.hWeights3, self.hOutput2)/self.nHidden
-        #self.hOutput3[:-1, :] = tanh(self.hActivation3)
-        #self.hOutput3[-1:, :] = 1.0
-         
-        # set bias neuron in hidden layer to 1.0
-        # self.hOutput[-1:, :] = 1.0
-         
-        # output layer
-        # print 'oweight', self.oWeights.shape
-        # print 'houtput3', self.hOutput3.shape
-        self.oActivation = dot(self.oWeights, self.hOutput2)/self.nOut
-        # print'oact', self.oActivation.shape
-        # self.oOutput = tanh(self.oActivation)
-        # Compute softmax on output activation
-        e = exp(self.oActivation)
+     #    # hidden layer
+     #    self.hActivation1 = dot(self.hWeights1, self.iOutput)/self.nHidden
+     #    self.hOutput1[:-1, :] = tanh(self.hActivation1)
+	# #print 'input eerste layer', self.hActivation1
+     #    self.hOutput1[-1:, :] = 1.0
+    #
+     #    self.hActivation2 = dot(self.hWeights2, self.hOutput1)/self.nHidden
+     #    self.hOutput2[:-1, :] = tanh(self.hActivation2)
+     #    self.hOutput2[-1:, :] = 1.0
+    #
+     #    #self.hActivation3 = dot(self.hWeights3, self.hOutput2)/self.nHidden
+     #    #self.hOutput3[:-1, :] = tanh(self.hActivation3)
+     #    #self.hOutput3[-1:, :] = 1.0
+     #
+     #    # set bias neuron in hidden layer to 1.0
+     #    # self.hOutput[-1:, :] = 1.0
+     #
+     #    # output layer
+     #    # print 'oweight', self.oWeights.shape
+     #    # print 'houtput3', self.hOutput3.shape
+     #    self.oActivation = dot(self.oWeights, self.hOutput2)/self.nOut
+     #    # print'oact', self.oActivation.shape
+     #    # self.oOutput = tanh(self.oActivation)
+     #    # Compute softmax on output activation
+        e = exp(self.activation)
         self.oOutput = e / sum(e)
 
     def backward(self, teach):
         self.correct[:,0] = teach
+        error = self.oOutput - self.correct
         # print 'corrects shape', self.correct.shape
-	#act = self.oActivation+0.0000000001
+	    #act = self.oActivation+0.0000000001
         #cost = -sum(self.correct*(log(act)))
         #print 'COST', self.cost()
-	error = self.oOutput - self.correct
+	    #error = self.oOutput - self.correct
         #cost2 = 0.0
         #for i in range(len(teach)):
             #cost2-= teach[i]*log(self.oActivation[i])
@@ -96,26 +102,26 @@ class FeedForwardNetwork:
          
         # deltas of output neurons
         self.oDelta = self.alpha * error
-	#print 'oDelta', self.oDelta
-                 
+	    #print 'oDelta', self.oDelta
+        self.weights = self.weights - dot(self.oDelta, self.iOutput.transpose())
         # deltas of hidden neurons
         #self.hDelta3 = (1 - tanh(self.hActivation3)* tanh(self.hActivation3)) * dot(self.oWeights[:,:-1].transpose(), self.oDelta)
-        self.hDelta2 = (1 - tanh(self.hActivation2)* tanh(self.hActivation2)) * dot(self.oWeights[:,:-1].transpose(), self.oDelta)
-        #print 'DELTA2'
-        #print self.hDelta2
-        self.hDelta1 = (1 - tanh(self.hActivation1)* tanh(self.hActivation1)) * dot(self.hWeights2[:,:-1].transpose(), self.hDelta2)
-        #print 'DELTA1'
-        #print self.hDelta1
-        # print 'oDelta', self.oDelta.shape
-        # print 'HDELTA1 SHAPE', self.hDelta3.shape
-        # print 'iOut shape', self.iOutput.shape
-        # print 'hWeight shape', self.hWeights.shape
-        # apply weight changes
-        self.hWeights1 = self.hWeights1 - dot(self.hDelta1, self.iOutput.transpose())
-        #print 'weights layer1', self.hWeights1
-	self.hWeights2 = self.hWeights2 - dot(self.hDelta2, self.hOutput1.transpose())
-        #self.hWeights3 = self.hWeights3 - self.alpha * dot(self.hDelta3, self.hOutput2.transpose())
-        self.oWeights = self.oWeights - dot(self.oDelta, self.hOutput2.transpose())
+        # self.hDelta2 = (1 - tanh(self.hActivation2)* tanh(self.hActivation2)) * dot(self.oWeights[:,:-1].transpose(), self.oDelta)
+        # #print 'DELTA2'
+        # #print self.hDelta2
+        # self.hDelta1 = (1 - tanh(self.hActivation1)* tanh(self.hActivation1)) * dot(self.hWeights2[:,:-1].transpose(), self.hDelta2)
+        # #print 'DELTA1'
+        # #print self.hDelta1
+        # # print 'oDelta', self.oDelta.shape
+        # # print 'HDELTA1 SHAPE', self.hDelta3.shape
+        # # print 'iOut shape', self.iOutput.shape
+        # # print 'hWeight shape', self.hWeights.shape
+        # # apply weight changes
+        # self.hWeights1 = self.hWeights1 - dot(self.hDelta1, self.iOutput.transpose())
+        # #print 'weights layer1', self.hWeights1
+        # self.hWeights2 = self.hWeights2 - dot(self.hDelta2, self.hOutput1.transpose())
+        # #self.hWeights3 = self.hWeights3 - self.alpha * dot(self.hDelta3, self.hOutput2.transpose())
+        # self.oWeights = self.oWeights - dot(self.oDelta, self.hOutput2.transpose())
 
     def cost(self):
 	act = self.oActivation + 0.0000001
@@ -123,29 +129,33 @@ class FeedForwardNetwork:
 	return cost
 
     def predict(self, Sample):
-        self.iOutput[:-1, 0] = Sample
-
-
+     #    self.iOutput[:-1, 0] = Sample
+    #
+    #
+     #    self.iOutput[-1:, 0] = 1.0
+    #
+     #    self.hActivation1 = dot(self.hWeights1, self.iOutput)/self.nHidden
+     #    self.hOutput1[:-1, :] = tanh(self.hActivation1)
+     #    self.hOutput1[-1:, :] = 1.0
+    #
+     #    self.hActivation2 = dot(self.hWeights2, self.hOutput1)/self.nHidden
+     #    self.hOutput2[:-1, :] = tanh(self.hActivation2)
+     #    self.hOutput2[-1:, :] = 1.0
+    #
+     #    #self.hActivation3 = dot(self.hWeights3, self.hOutput2)/self.nHidden
+     #    #self.hOutput3[:-1, :] = tanh(self.hActivation3)
+     #    #self.hOutput3[-1:, :] = 1.0
+	# #print 'hOutput', self.hOutput
+    #
+     #    # set bias neuron in hidden layer to 1.0
+     #    # self.hOutput[-1:, :] = 1.0
+    #
+     #    # output layer
+     #    self.oActivation = dot(self.oWeights, self.hOutput2)/self.nOut
+        self.iOutput[:-1, 0] = input
         self.iOutput[-1:, 0] = 1.0
 
-        self.hActivation1 = dot(self.hWeights1, self.iOutput)/self.nHidden
-        self.hOutput1[:-1, :] = tanh(self.hActivation1)
-        self.hOutput1[-1:, :] = 1.0
-
-        self.hActivation2 = dot(self.hWeights2, self.hOutput1)/self.nHidden
-        self.hOutput2[:-1, :] = tanh(self.hActivation2)
-        self.hOutput2[-1:, :] = 1.0
-
-        #self.hActivation3 = dot(self.hWeights3, self.hOutput2)/self.nHidden
-        #self.hOutput3[:-1, :] = tanh(self.hActivation3)
-        #self.hOutput3[-1:, :] = 1.0
-	#print 'hOutput', self.hOutput
-
-        # set bias neuron in hidden layer to 1.0
-        # self.hOutput[-1:, :] = 1.0
-
-        # output layer
-        self.oActivation = dot(self.oWeights, self.hOutput2)/self.nOut
+        self.activation = dot(self.weights, self.iOutput)
         e = exp(self.oActivation)
         self.oOutput = e / sum(e)
         # self.oOutput = self.oActivation
