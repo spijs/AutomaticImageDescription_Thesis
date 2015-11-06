20dafcb2ec844f6d30f8916446612a11f3693b12
Wout <w.vekemans@gmail.com>
2015-11-06 12:12:04 +0100

    test file for topic reading

---
+++ test_validation_topic_read.py
@@ -0,0 +1,14 @@
+__author__ = 'Wout'
+
+import lda
+from imagernn.data_provider import getDataProvider
+import numpy as np
+
+if __name__ == "__main__":
+    dataprovider = getDataProvider('flickr30k', 120)
+
+    dataprovider.load_topic_models('flickr30k', 120)
+
+    top = dataprovider.topics
+    print "amount of topics", len(top)
+
