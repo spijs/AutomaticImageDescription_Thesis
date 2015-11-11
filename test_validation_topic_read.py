4ede2a5513f5e9c14a6227897d194cfe519c9072
Thijs Dieltjens <r0299068@paris.cs.kuleuven.be>
2015-11-11 14:08:03 +0100

    test file updated

--- test_validation_topic_read.py
+++ test_validation_topic_read.py
@@ -5,13 +5,13 @@ from imagernn.data_provider import getDataProvider
 import numpy as np
 
 if __name__ == "__main__":
-    dataprovider = getDataProvider('flickr30k', 120)
+    dataprovider = getDataProvider('flickr30k')
 
     dataprovider.load_topic_models('flickr30k', 120)
 
     dataprovider.iterImageSentencePairBatch(split = 'val')
 
     top = dataprovider.topics
-    print 'imagenames', top.keys
+    print 'imagenames', top.keys()
     print "amount of topics", len(top)
 
