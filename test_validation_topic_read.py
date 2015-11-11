2d2899fec13308e719aa05acfb3a83e18daa3285
Thijs Dieltjens <Thijs Dieltjens>
2015-11-11 14:59:01 +0100

    test

--- test_validation_topic_read.py
+++ test_validation_topic_read.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     dataprovider = getDataProvider('flickr30k')
     dataprovider.load_topic_models('flickr30k', 120)
     batches = dataprovider.iterImageSentencePairBatch(split = 'val')
-    i=0
     for batch in batches:
-        print i
-        i+=1
         print (x['topics'] for x in batch)
+        print(batch)
