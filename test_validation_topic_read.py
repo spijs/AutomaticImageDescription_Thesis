39fb8e386f0299b728547c36b52f1c4e8c72c440
Thijs Dieltjens <Thijs Dieltjens>
2015-11-11 14:56:54 +0100

    weird

--- test_validation_topic_read.py
+++ test_validation_topic_read.py
@@ -8,5 +8,8 @@ if __name__ == "__main__":
     dataprovider = getDataProvider('flickr30k')
     dataprovider.load_topic_models('flickr30k', 120)
     batches = dataprovider.iterImageSentencePairBatch(split = 'val')
+    i=0
     for batch in batches:
+        print i
+        i+=1
         print (x['topics'] for x in batch)
