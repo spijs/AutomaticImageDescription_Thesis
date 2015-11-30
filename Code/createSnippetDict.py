0fba796fa0b73299dc38e0396a293ca6299ca564
Wout <w.vekemans@gmail.com>
2015-11-30 21:50:27 +0100

    check image size in dict creation

--- Code/createSnippetDict.py
+++ Code/createSnippetDict.py
@@ -1,5 +1,22 @@
 import os
 from nltk.stem.porter import *
+from PIL import Image
+
+'''
+Given a filename, checks if the image behind that filename is bigger than 64x64
+'''
+def isLargeEnough(filename):
+    file = filename+".jpg"
+    print file
+    try:
+        image = Image.open("./Flickr30kEntities/image_snippets/"+file)
+    except IOError:
+
+	print "IMG NOT FOUND"
+	return False
+    width, height = image.size
+    print width,height
+    return (width >= 64) and (height >= 64)
 
 ''' stems a word by using the porter algorithm'''
 def stem(word):
@@ -23,18 +40,19 @@ if __name__ == "__main__":
         for filename in filenames:
             current += 1
             print "Preprocessing sentence: " + str(current)
-            f= open('./Flickr30kEntities/sentence_snippets/'+filename)
-            line = f.readline()
-            # print filename
-            while not (line == ""):
-                for word in line.split():
-                    word = stem(word.decode('utf-8'))
-                    if (not word in stopwords):
-                        if(not word in dict):
-                            dict[word]=1
-                        else:
-                            dict[word]+=1
+            if isLargeEnough('./Flickr30kEntities/image_snippets/'+filename[0:-4]+'.jpg'):
+                f= open('./Flickr30kEntities/sentence_snippets/'+filename)
                 line = f.readline()
+                # print filename
+                while not (line == ""):
+                    for word in line.split():
+                        word = stem(word.decode('utf-8'))
+                        if (not word in stopwords):
+                            if(not word in dict):
+                                dict[word]=1
+                            else:
+                                dict[word]+=1
+                    line = f.readline()
         for word in dict:
             if(dict[word] >= 5):
                 result[word]=dict[word]
