c1f741a0a06247620f72c567b43610c984dd1d6c
Wout <w.vekemans@gmail.com>
2015-12-01 01:00:24 +0100

    more pirnts

--- Code/createSnippetVectors.py
+++ Code/createSnippetVectors.py
@@ -86,11 +86,12 @@ def weight_tfidf(documents, inv_freq):
     for i in documents.keys():
         doc = documents[i]
         result[i] = doc * inv_freq
-        try:
-            float(result[i])
-        except ValueError:
-            print "NO FLOAT G"
-            print str(result[i])
+        for j in range(len(result[i])):
+            try:
+                float(result[i][j])
+            except ValueError:
+                print "NO FLOAT G"
+                print str(result[i])
     return result
 
 def mainExec(name_file, features):
