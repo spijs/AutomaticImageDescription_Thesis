7b22e0ad82ed71b6714db1a0276c0e36db7fd398
Thijs Dieltjens <Thijs Dieltjens>
2015-12-02 19:54:31 +0100

    fixed guide

--- Code/imagernn/Guide.py
+++ Code/imagernn/Guide.py
@@ -1,16 +1,13 @@
 __author__ = 'Wout&Thijs'
 
+def get_guide_size(guide_type):
+    if guide_type=="image":
+        return 256
 
-class guide:
+def get_guide(guide_type,im):
+    if guide_type=="image":
+        return get_image_guide(im)
 
-    def get_guide_size(guide_type):
-        if guide_type=="image":
-            return 256
-
-    def get_guide(self,guide_type,im):
-        if guide_type=="image":
-            return self.get_image_guide(im)
-
-    def get_image_guide(self,im):
-        return im
+def get_image_guide(im):
+    return im
 
