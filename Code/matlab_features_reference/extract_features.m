8011331bbd0cf0ae283af6bb95e0397ec04bff69
Thijs Dieltjens <r0299068@paris.cs.kuleuven.be>
2015-12-02 11:50:02 +0100

    geen caffe error

--- Code/matlab_features_reference/extract_features.m
+++ Code/matlab_features_reference/extract_features.m
@@ -1,6 +1,6 @@
 %% vgg / caffe spec
 %% Runnen met matlab extract_features.m
-path(path,'/export/home2/NoCsBack/hci/wout/caffe/matlab/caffe')
+path(path,'/export/home2/NoCsBack/hci/wout/caffe/matlab')
 path(path,'matlab_features_reference')
 
 use_gpu = 0;
