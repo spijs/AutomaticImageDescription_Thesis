4bc1040043cb9b1c6fec7b700ee91929b274d69b
Wout <w.vekemans@gmail.com>
2015-12-05 13:23:20 +0100

    creating restricted imagelist

---
+++ Code/createImageList.py
@@ -0,0 +1,33 @@
+__author__ = 'wout'
+
+import os
+import numpy as np
+from nltk.stem.porter import *
+from sklearn.cross_decomposition import CCA
+import scipy.io
+from scipy import spatial
+import pickle
+from PIL import Image
+from imagernn.data_provider import getDataProvider
+from stackedCCAModel import *
+
+'''
+Given a filename, checks if the image behind that filename is bigger than 64x64
+'''
+def isLargeEnough(filename):
+    file = filename+".jpg"
+    #print file
+    try:
+        image = Image.open("Flickr30kEntities/image_snippets/"+file)
+    except IOError:
+        # image not found. Is ok, many snippets dont have a corresponding image
+	    return False
+    width, height = image.size
+    return (width >=400 ) and (height >= 400)
+
+if __name__ == "__main__":
+    for dirname, dirnames, filenames in os.walk('Flickr30kEntities/image_snippets'):
+        f = open("Flickr30kEntities/images_snippets/images.txt", 'w+')
+        for filename in filenames:
+            if isLargeEnough(filename):
+                f.write(filename+'\n')
