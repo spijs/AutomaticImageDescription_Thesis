d3d109f0d0fc0f3255911c98fd12008f72e54a25
Thijs Dieltjens <r0299068@paris.cs.kuleuven.be>
2015-12-06 13:31:11 +0100

    why wasnt this file pushed

--- Code/createImageList.py
+++ Code/createImageList.py
@@ -15,7 +15,7 @@ from stackedCCAModel import *
 Given a filename, checks if the image behind that filename is bigger than 64x64
 '''
 def isLargeEnough(filename):
-    file = filename+".jpg"
+    file = filename
     #print file
     try:
         image = Image.open("Flickr30kEntities/image_snippets/"+file)
@@ -23,11 +23,11 @@ def isLargeEnough(filename):
         # image not found. Is ok, many snippets dont have a corresponding image
 	    return False
     width, height = image.size
-    return (width >=400 ) and (height >= 400)
+    return (width >=64 ) and (height >= 64)
 
 if __name__ == "__main__":
+    f = open("Flickr30kEntities/image_snippets/images.txt",'w+')
     for dirname, dirnames, filenames in os.walk('Flickr30kEntities/image_snippets'):
-        f = open("Flickr30kEntities/images_snippets/images.txt", 'w+')
         for filename in filenames:
             if isLargeEnough(filename):
                 f.write(filename+'\n')
