78cdf06fca078f733409d1f5981234c807999851
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-02-09 12:54:48 +0100

    refactoring + added first meteor

---
+++ Code/eval/evaluationStrategy.py
@@ -0,0 +1,14 @@
+__author__ = 'spijs'
+
+class EvaluationStrategy:
+    def __init__(self,name):
+        self.name = name
+
+    def evaluate_sentence(self,sentence,references,n):
+        pass
+
+    def evaluate_total(self,sentences,references,n):
+        pass
+
+    def get_name(self):
+        return self.name
