ff391cb9c179407aa9eff199cd6f691413bdef86
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-02-20 13:21:34 +0100

    fix

--- Code/imagernn/generic_batch_generator.py
+++ Code/imagernn/generic_batch_generator.py
@@ -211,9 +211,7 @@ class GenericBatchGenerator:
     """ some code duplication here with forward pass, but I think we want the freedom in future """
     F = np.row_stack(x['image']['feat'] for x in batch)
     lda_enabled = params.get('lda',0)
-    L = np.zeros((params.get('image_encoding_size',128),lda_enabled))
-    if lda_enabled:
-       L = np.row_stack(x['topics'] for x in batch)
+    L = topics
     We = model['We']
     #Wlda = model['Wlda']
     be = model['be']
