38e6d4e63ba1106cd6df47cd175286213e3144f3
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-03-03 21:25:23 +0100

    er was niks fout, enkel foute waarde in oude pickle file...

--- Code/imagernn/generic_batch_generator.py
+++ Code/imagernn/generic_batch_generator.py
@@ -243,9 +243,9 @@ class GenericBatchGenerator:
     for i,x in enumerate(batch):
       Xi = Xe[i,:]
       guide = get_guide(guide_input,F[i,:],L=L[i,:])
-      #if lda_enabled and not guide_input:
-      guide = lda[i,:]
-      #  print 'guide = lda'
+      if lda_enabled and not guide_input:
+        guide = lda[i,:]
+        print 'guide = lda'
       gen_Y = Generator.predict(Xi, guide, model, model['Ws'], params, **kwparams)
       Ys.append(gen_Y)
     return Ys
