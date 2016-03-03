f1cbb81d6fdfe0a70669a258f87b4e0377f95a65
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-03-03 21:15:24 +0100

    prints

--- Code/imagernn/generic_batch_generator.py
+++ Code/imagernn/generic_batch_generator.py
@@ -216,6 +216,7 @@ class GenericBatchGenerator:
       guide = get_guide(guide_input,F[i,:],L=L[i,:])
       if lda_enabled and not guide_input:
         guide = lda[i,:]
+        print 'guide = lda'
       gen_Y = Generator.predict(Xi, guide, model, model['Ws'], params, **kwparams)
       Ys.append(gen_Y)
     return Ys
