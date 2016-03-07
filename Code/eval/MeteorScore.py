7d281e8ff4d5b27ab75035c10a08e48a11d84386
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-03-07 16:51:35 +0100

    removed .txt

--- Code/eval/MeteorScore.py
+++ Code/eval/MeteorScore.py
@@ -26,7 +26,7 @@ class MeteorScore(EvaluationStrategy):
          self.write_references(references)
          self.write_sentences(sentences)
 
-         command = "java -Xmx2G -jar meteor/meteor-1.5.jar meteor_sentences.txt meteor_references.txt -r 5 -l en -norm"
+         command = "java -Xmx2G -jar meteor/meteor-1.5.jar meteor_sentences meteor_references -r 5 -l en -norm"
          process = sp.Popen(command,stdin=sp.PIPE, stdout=sp.PIPE, shell=True)
          lines_iterator = iter(process.stdout.readline, b"")
          fline = ""
