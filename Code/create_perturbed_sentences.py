620d1103bdbad7dcb6bf3741b79cbddf8a6d12a0
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-03-10 13:08:12 +0100

    create vocabulary

--- Code/create_perturbed_sentences.py
+++ Code/create_perturbed_sentences.py
@@ -1,1 +1,29 @@
 __author__ = 'spijs'
+
+__author__ = 'Wout & thijs'
+
+
+from imagernn.data_provider import getDataProvider
+
+
+def create_vocabulary(params):
+    dataset = 'flickr30k'
+    dataprovider = getDataProvider(dataset)
+    img_sentence_pair_generator = dataprovider.iterImageSentencePair()
+    dict = {}
+    result = []
+    for pair in img_sentence_pair_generator:
+        sentence = pair['sentence']['tokens']
+        for word in sentence:
+            word = word.decode('utf-8').lower()
+            if(not word in dict):
+                dict[word]=1
+            else:
+                dict[word]+=1
+    for word in dict:
+            if(dict[word] >= 5):
+                result.append(dict[word])
+    f = open("vocabulary.txt", "w+")
+    for w in result:
+        f.writelines(w+'\n')
+    print('created vocabulay')
