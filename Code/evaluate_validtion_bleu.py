3773540ecf7b293277951f8fc5691d83a29891df
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-03-14 17:45:16 +0100

    evaluate validation set with bleu score

--- Code/evaluate_validtion_bleu.py
+++ Code/evaluate_validtion_bleu.py
@@ -28,73 +28,73 @@ def main(params):
 
   best_bleu = 0
   best = ""
-  _,_,files = os.walk(checkpoint_path)
-  for f in files:
+  for _,_,files in os.walk(checkpoint_path):
+      for f in files:
 
-      print 'loading checkpoint %s' % (f, )
-      checkpoint = pickle.load(open(checkpoint_path+'/'+f, 'rb'))
-      checkpoint_params = checkpoint['params']
-      model = checkpoint['model']
+          print 'loading checkpoint %s' % (f, )
+          checkpoint = pickle.load(open(checkpoint_path+'/'+f, 'rb'))
+          checkpoint_params = checkpoint['params']
+          model = checkpoint['model']
 
-      misc = {}
-      misc['wordtoix'] = checkpoint['wordtoix']
-      ixtoword = checkpoint['ixtoword']
-
-
-      # iterate over all images in test set and predict sentences
-      BatchGenerator = decodeGenerator(checkpoint_params)
-      n = 0
-      all_references = []
-      all_candidates = []
-
-      if params['cca']:
-        ccaweights = np.loadtxt('cca/imageprojection_'+str(params['cca'])+'.txt', delimiter = ',')
-        misc['ccaweights'] = ccaweights
-      else:
-        ccaweights = None
-      for img in dp.iterImages(split = 'val', max_images = max_images):
-        n+=1
-        print 'image %d/%d:' % (n, max_images)
-        references = [' '.join(x['tokens']) for x in img['sentences']] # as list of lists of tokens
-        kwparams = { 'beam_size' : params['beam_size'], 'normalization': params['normalization'], 'ccaweights' : ccaweights }
-        if not params['lda'] == 0:
-            Ys = BatchGenerator.predict_test([{'image':img}], model, checkpoint_params, **kwparams)
-        else:
-            Ys = BatchGenerator.predict_test([{'image':img}], model, checkpoint_params, **kwparams)
+          misc = {}
+          misc['wordtoix'] = checkpoint['wordtoix']
+          ixtoword = checkpoint['ixtoword']
 
 
-        for gtsent in references:
-          print 'GT: ' + gtsent
+          # iterate over all images in test set and predict sentences
+          BatchGenerator = decodeGenerator(checkpoint_params)
+          n = 0
+          all_references = []
+          all_candidates = []
 
-        # now evaluate and encode the top prediction
-        top_predictions = Ys[0] # take predictions for the first (and only) image we passed in
-        top_prediction = top_predictions[0] # these are sorted with highest on top
-        candidate = ' '.join([ixtoword[ix] for ix in top_prediction[1] if ix > 0]) # ix 0 is the END token, skip that
-        print 'PRED: (%f) %s' % (top_prediction[0], candidate)
+          if params['cca']:
+            ccaweights = np.loadtxt('cca/imageprojection_'+str(params['cca'])+'.txt', delimiter = ',')
+            misc['ccaweights'] = ccaweights
+          else:
+            ccaweights = None
+          for img in dp.iterImages(split = 'val', max_images = max_images):
+            n+=1
+            print 'image %d/%d:' % (n, max_images)
+            references = [' '.join(x['tokens']) for x in img['sentences']] # as list of lists of tokens
+            kwparams = { 'beam_size' : params['beam_size'], 'normalization': params['normalization'], 'ccaweights' : ccaweights }
+            if not params['lda'] == 0:
+                Ys = BatchGenerator.predict_test([{'image':img}], model, checkpoint_params, **kwparams)
+            else:
+                Ys = BatchGenerator.predict_test([{'image':img}], model, checkpoint_params, **kwparams)
 
-        # save for later eval
-        all_references.append(references)
-        all_candidates.append(candidate)
 
-      # use perl script to eval BLEU score for fair comparison to other research work
-      # first write intermediate files
-      print 'writing intermediate files into eval/'
-      open('eval/output', 'w').write('\n'.join(all_candidates))
-      for q in xrange(5):
-        open('eval/reference'+`q`, 'w').write('\n'.join([x[q] for x in all_references]))
-      # invoke the perl script to get BLEU scores
-      print 'invoking eval/multi-bleu.perl script...'
-      owd = os.getcwd()
-      os.chdir('eval')
-      process = sp.Popen('./multi-bleu.perl reference < output',stdin=sp.PIPE, stdout=sp.PIPE, shell=True)
-      lines_iterator = iter(process.stdout.readline, b"")
-      for line in lines_iterator:
-            fourth = line.split("/")[4]
-            bleu_4  = fourth.split(" ")[0]
-      if(bleu_4>best_bleu):
-          best = f
-          best_bleu = bleu_4
-      os.chdir(owd)
+            for gtsent in references:
+              print 'GT: ' + gtsent
+
+            # now evaluate and encode the top prediction
+            top_predictions = Ys[0] # take predictions for the first (and only) image we passed in
+            top_prediction = top_predictions[0] # these are sorted with highest on top
+            candidate = ' '.join([ixtoword[ix] for ix in top_prediction[1] if ix > 0]) # ix 0 is the END token, skip that
+            print 'PRED: (%f) %s' % (top_prediction[0], candidate)
+
+            # save for later eval
+            all_references.append(references)
+            all_candidates.append(candidate)
+
+          # use perl script to eval BLEU score for fair comparison to other research work
+          # first write intermediate files
+          print 'writing intermediate files into eval/'
+          open('eval/output', 'w').write('\n'.join(all_candidates))
+          for q in xrange(5):
+            open('eval/reference'+`q`, 'w').write('\n'.join([x[q] for x in all_references]))
+          # invoke the perl script to get BLEU scores
+          print 'invoking eval/multi-bleu.perl script...'
+          owd = os.getcwd()
+          os.chdir('eval')
+          process = sp.Popen('./multi-bleu.perl reference < output',stdin=sp.PIPE, stdout=sp.PIPE, shell=True)
+          lines_iterator = iter(process.stdout.readline, b"")
+          for line in lines_iterator:
+                fourth = line.split("/")[4]
+                bleu_4  = fourth.split(" ")[0]
+          if(bleu_4>best_bleu):
+              best = f
+              best_bleu = bleu_4
+          os.chdir(owd)
   print 'Best bleu score for %s : %s' % (best,best_bleu)
 
 if __name__ == "__main__":
