bea64ba5b2e3714ae97c118c4cb046e14645b7be
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-04-16 14:15:39 +0200

    print idf$

--- Code/print_idf.py
+++ Code/print_idf.py
@@ -7,8 +7,10 @@ def main():
     input = open('idf.p','rb')
     idf = pickle.load(input)
     sorted_idf = sorted(idf.items(), key=operator.itemgetter(1))
+    output = open('sorted_idf.txt','w')
     for pair in sorted_idf:
         print "Word: " + pair[0] + " IDF: " + str(pair[1])
-
+        output.write(pair[0] + ": " + str(pair[1]))
+    output.close()
 if __name__ == "__main__":
     main()
