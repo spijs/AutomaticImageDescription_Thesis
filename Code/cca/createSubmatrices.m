889c938f1ec2c8ff5ff7dfedfef8a5fefe4479b5
Wout <w.vekemans@gmail.com>
2016-05-24 18:49:40 +0200

    m file and thesis

--- Code/cca/createSubmatrices.m
+++ Code/cca/createSubmatrices.m
@@ -1,7 +1,8 @@
+% read a matrix from disk and create 4 submatrices, based on an array of sizes.
 function createSubmatrices()
   projection = dlmread('imageprojection_pert.txt', ',');
   for k = [128 256 512 1024]
-    submatrix = projection(:,1:k);
+    submatrix = projection(:,1:k); % take the k first columns of the matrix
     filename = strcat('imageprojection_',int2str(k),'_pert.txt');
     dlmwrite(filename, submatrix);
   end
